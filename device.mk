c8c152b91e2c87f060f8e0c813ec91e7fdd813e9
Vachounet <vachounet@live.fr>
2017-08-23 13:03:02 +0200

    perry: Build Gatekeeper HAL
    Change-Id: If80e0b48e0c12fbe601b55416004bc5ee649657a
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8937
